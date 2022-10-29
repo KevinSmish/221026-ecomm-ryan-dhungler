0376034a5d3198f8e57d6ec7af0e8873cc52729e
KevinSmith <!QAZt85y7g^YHN>
2022-10-29 12:45:44 +0300

    add mongodb models

---
+++ server/controllers/authController.js
@@ -0,0 +1,3 @@
+export const users = async (req, res, next) => {
+  res.json({ data: "Hello, world?" });
+};
--- server/index.js
+++ server/index.js
@@ -1,12 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+//import User from "../models/user.js";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 
 const app = express();
-app.get("/users", (req, res, next) => {
-  res.json({ data: "Hello, world!!" });
-});
+
+// db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("DB Connected"))
+  .catch((err) => console.log("DB ERROR => ", err));
+
+// routes middleware
+app.use("/api", authRoutes);
 
 const PORT = process.env.PORT || 3000;
 
